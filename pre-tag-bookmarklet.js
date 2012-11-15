103bbb3693225eca4fded81a799626ae4fd0d849
Dan Farrow <dan@squarebracket.net>
2012-11-15 10:02:23 -0500

    first commit

---
+++ pre-tag-bookmarklet.js
@@ -0,0 +1,3 @@
+//javascript:(function(){window.document.body.innerHTML='<pre>'+window.document.body.innerHTML;})();
+
+(function(){window.document.body.innerHTML='<pre>'+window.document.body.innerHTML;})();
